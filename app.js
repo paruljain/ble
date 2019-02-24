4ad9a94d5ac2f65bcbc9898b77af73ab41ffcbb1
Parul Jain <paruljain@hotmail.com>
2019-02-23 23:03:42 -0500

    first

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+noble.on('stateChange', function(state) {
+    if (state === 'poweredOn') {
+      noble.startScanning()
+    } else {
+      noble.stopScanning()
+    }
+  });
+  
+  noble.on('discover', function(peripheral) {
+      console.log('Found device with local name: ' + peripheral.advertisement.localName)
+      console.log('advertising the following service uuid\'s: ' + peripheral.advertisement.serviceUuids)
+      console.log()
+  })
